226135696399f13992278dbd803493ae77074d6f
Luke Kanies <luke@madstop.com>
2008-07-08 07:23:24 +0200

    Adding copyright info
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- manifests/tracsite.pp
+++ manifests/tracsite.pp
@@ -1,3 +1,17 @@
+# Copyright (c) 2008, Luke Kanies, luke@madstop.com
+# 
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 # Install an Apache configuration for this Trac instance.
 # This assumes your apache instance is configured to load everything
 # in /etc/apache2/trac, and probably only works on Debian.
@@ -18,5 +32,3 @@ define tracsite($cgidir, $tracdir) {
         ensure => "/usr/share/trac/cgi-bin/trac.cgi"
     }
 }
-
-# $Id$
